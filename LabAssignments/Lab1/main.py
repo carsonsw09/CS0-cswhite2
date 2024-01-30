d84d178ed5913eaf38c23b56d1b8006f63f14657
Carson <carsonsw09@gmail.com>
2024-01-30 16:52:27 -0700

    Final Submission

---
+++ LabAssignments/Lab1/main.py
@@ -0,0 +1,57 @@
+"""
+    StdIO Lab
+    ASCII Art - using literals and variables
+    
+    Updated By: <Carson White> #FIXME1  Fixed
+    Date: 1/30/24 #FIXME2  Fixed
+    
+    This program produces an ASCII art on the console.
+
+    Algorithm steps: 
+    1. Use variables to store data/values
+    2. Write a series of print statements to print the data/values to the console
+"""
+
+print("Welcome to ASCII Art Program...\n")
+
+# FIXME3: prompt user to enter their name and store the value into name variable using input function  Fixed
+name: str = input("Please enter your name ")
+print("Nice to meet you " + name + " Welcome to the program.\n ")
+# FIXME4: greet the name using the variable as the following output  Fixed
+# must output: Nice meeting you, <name>!
+
+# prompt user to enter the semester and store the value into semester variable using input function
+semester: str = input("What semester is this [Fall/Spring]? ")
+print("This is " + semester + " semester.\n")
+
+# FIXME5: prompt user to enter the year and store the value into year variable using input function fixed
+year: str = input("Please enter the year ")
+print("This is " + year + " year ")
+# FIXME6: print the year using the variable as the following output  fixed
+# must output: This is <year> year.
+
+print("Hope you like my ASCII art...\n\n")
+
+line1: str = "  |\\_/|   ***********************    (\\_/)\n"
+print(line1)
+
+# FIXME7: use variable to print the second line of the graphic fixed
+secondline = " / @ @ \  *        ASCII Lab    *    (='.'=)"
+print(secondline)
+
+thirdline = "( > 0 < ) *       <"+ name + ">      *  ( \" )( \" )"
+print(thirdline)                                                                  
+# FIXME8: print the third line of the graphics fixed
+fourthline = "  >>x<<   *     <" + semester + year +">    *"
+print(fourthline)
+# FIXME9: use variable to print the fourth line Fixed
+# FIXME10: use variable to print the fifth line
+fifthline = " /  O  \  *        CSCI 111     * "
+print(fifthline)
+
+sixthline = "          ***********************     "
+print(sixthline)
+
+# Note: You can add more lines or print more ASCII arts of your choice if you'd like...
+
+print("\nGood bye...  \n")
